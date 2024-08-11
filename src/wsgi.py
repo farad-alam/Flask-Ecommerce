39cb03d4cb06a6eb90b4e387607e6754c2ef3b19
Farad Alam <farad.dev@gmail.com>
2024-08-11 21:35:22 +0600

    add logging and remove src from import path

--- src/wsgi.py
+++ src/wsgi.py
@@ -2,7 +2,12 @@ import os
 import sys
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from src.ecommerce import app, db 
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+logging.debug(sys.path)
+
+from ecommerce import app, db 
 
 
 if __name__ == "__main__":
